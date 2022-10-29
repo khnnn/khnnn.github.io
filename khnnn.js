dda667f267ba8c3c6b9ca22bfc32dd4929180f88
khnnn <116963060+khnnn@users.noreply.github.com>
2022-10-29 23:05:51 +0800

    Update khnnn.js

--- khnnn.js
+++ khnnn.js
@@ -1,1 +1,28 @@
-
+<script>
+    const circ = document.querySelectorAll('.btn');
+    const txt = document.querySelectorAll('.text');
+    const bg = document.querySelectorAll('.bg');
+    let i = 0;
+    circ[0].addEventListener('click',()=>{
+        circ[1].classList.toggle("inc");
+        circ[0].classList.add("inc");
+        bg[i].classList.add('inc');
+        i++;
+        if(i=>1){
+            txt[i-1].classList.add('hide');
+            txt[i].classList.remove('hide');
+            if(i==3){
+                circ[0].addEventListener('click',()=>{
+                    txt[0].classList.remove('hide');
+                    setTimeout(function(){
+                        window.location.reload();
+                    },1000);
+                })
+            }
+        }
+        else{
+            txt[0].classList.add('hide');
+            txt[i].classList.remove('hide');
+        }
+    })
+</script>
